a295ca51698e41b2ef5b8d6093ea7362e8e3c054
Sal Ferrarello <sal@ironcodestudio.com>
2017-04-14 12:00:17 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Iron Code Recipe Custom Post Type
+
+This plugin powers the recipe section on https://ferrarello.com/recipes.
+
+It also serves as a companion plugin to my blog post
+[WordPress CPT Best Practices](https://salferrarello.com/cpt-best-practices/),
+which was created as a companion post to my WordCamp Lehigh Valley 2016
+presentation [Best Practices for Creating Custom Post Types](http://wordpress.tv/2016/08/31/sal-ferrarello-best-practices-for-creating-custom-post-types/).
+
+This plugin can be found at https://github.com/salcode/cpt-fe-recipe
+
+## Author
+
+[Sal Ferrarello](https://salferrarello.com/) / [@salcode](https://twitter.com/salcode)
---
+++ cpt-fe-recipe.php
@@ -0,0 +1,91 @@
+<?php
+/**
+ * Plugin Name: Iron Code Recipe Custom Post Type
+ * Plugin URI: https://salferrarello.com/cpt-best-practices/
+ * Description: Register a WordPress Custom Post Type (CPT) `fe_recipe` with a custom Taxonomy `fe_recipe_tag`.
+ * Version: 1.3.0
+ * Author: Sal Ferrarello
+ * Author URI: http://salferrarello.com/
+ * Text Domain: fe-recipe-cpt
+ * Domain Path: /languages
+ *
+ * @package fe-recipe-cpt
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+register_activation_hook(   __FILE__, 'fe_recipe_cpt_activation' );
+register_deactivation_hook( __FILE__, 'fe_recipe_cpt_deactivation' );
+
+add_action( 'init', 'fe_recipe_cpt' );
+
+/**
+ * Register a public CPT and Taxonomy
+ */
+function fe_recipe_cpt() {
+
+	// Post type should be prefixed, singular, and no more than 20 characters.
+	register_post_type( 'fe_recipe', array(
+		// Label should be plural and L10n ready.
+		'label'       => __( 'Recipes', 'fe_recipe' ),
+		'public'      => true,
+		'has_archive' => true,
+		'rewrite'     => array(
+			// Slug should be plural and L10n ready.
+			'slug'        => _x( 'recipes', 'CPT permalink slug', 'fe_recipe' ),
+			'with_front'  => false,
+		),
+
+		/**
+		 * 'title', 'editor', 'thumbnail' 'author', 'excerpt','custom-fields',
+		 * 'page-attributes' (menu order),'revisions' (will store revisions),
+		 * 'trackbacks', 'comments', 'post-formats',
+		 */
+		'support'     => array( 'title', 'editor' ),
+
+		// Url to icon or choose from built-in https://developer.wordpress.org/resource/dashicons/.
+		'menu_icon'   => 'dashicons-feedback',
+	) );
+
+	register_taxonomy(
+		'fe_recipe_tag',
+		'fe_recipe',
+		array(
+			// Label should be plural and L10n ready.
+			'label'             => __( 'Recipe Tags', 'fe_recipe' ),
+			'show_admin_column' => true,
+			'rewrite'           => array(
+				// Slug should be singular and L10n ready..
+				'slug' => _x( 'recipe-tag', 'Custom Taxonomy slug', 'fe_recipe' ),
+			),
+		)
+	);
+}
+
+/**
+ * Load Recipe Custom Post Type and Flush Rewrite Rules
+ *
+ * We run this on plugin activation to prevent the problem of the custom post
+ * type URLs not loading initially (because their URL pattern is not included
+ * in the cached rewrite rules). We explicitly call the code to register the
+ * custom post type because that code, which executes on the `init`, hook
+ * has not yet executed.
+ */
+function fe_recipe_cpt_activation() {
+	fe_recipe_cpt();
+	flush_rewrite_rules();
+}
+
+/**
+ * Flush the rewrite rules.
+ *
+ * We run this on plugin deactivation to ensure the rewrite rules no longer
+ * included the URL pattern for our Custom Post Type.
+ */
+function fe_recipe_cpt_deactivation() {
+	flush_rewrite_rules();
+	flush_rewrite_rules();
+}
